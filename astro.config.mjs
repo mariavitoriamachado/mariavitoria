315998bbddb247a7fde6974ae68d303a0a717cc7
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2023-02-21 14:09:07 -0300

    fix: imagens and cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: cloudflare(),
   integrations: [mdx(), sitemap(), tailwind()]
 });
