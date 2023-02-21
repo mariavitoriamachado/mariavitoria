6e511e3f377456aca5fb9f29ac5994fe76cba736
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2023-02-21 13:15:39 -0300

    chore:  add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({ mode: "directory" }),
+  adapter: cloudflare(),
   integrations: [mdx(), sitemap(), tailwind()]
 });
