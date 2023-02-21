3d9a3dd7cdd1f4a345aad8dd14142b9adb21c8ab
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2023-02-21 19:38:04 -0300

    chore: adjust CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import cloudflare from '@astrojs/cloudflare';
-
-// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
