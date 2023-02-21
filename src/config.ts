2dcc97fc1fd6c9018a4583047e7917719d1e4996
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2023-02-21 13:34:42 -0300

    chore: adjust

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Maria Vitória';
-export const SITE_DESCRIPTION = 'https://mariavitoriasilva.github.io/mariavitt/';
+export const SITE_DESCRIPTION = 'Site pessoal';
